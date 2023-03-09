6982fb9ca6ba1731f70a490cf3b12c3695493378
jamulya <jamka_kg@mail.ru>
2023-03-09 20:14:07 +0600

    lesson1

---
+++ src/components/pages/starships-page.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import Row from '../row';
+
+
+const StarshipsPage = () => {
+    const [state, setState] = useState({itenId: 2})
+
+
+  return (
+    // <Row left={leftElement} right={rightElement} />
+    <div></div>
+  )
+}
+
+export default StarshipsPage ;
